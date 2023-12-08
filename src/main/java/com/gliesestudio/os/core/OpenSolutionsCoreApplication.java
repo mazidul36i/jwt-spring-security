a479e1a02359db555866e182691f77472e166671
MazidulIslam <mazidul.islam@prospecta.com>
2023-12-08 17:49:26 +0530

    initial commit

---
+++ src/main/java/com/gliesestudio/os/core/OpenSolutionsCoreApplication.java
@@ -0,0 +1,13 @@
+package com.gliesestudio.os.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenSolutionsCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenSolutionsCoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gliesestudio/os/core/OpenSolutionsCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gliesestudio.os.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenSolutionsCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
